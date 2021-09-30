e1ecba0a7296f120bba4ec08e6504d5daf681650
bosh <74213741+joshrab@users.noreply.github.com>
2021-09-30 09:58:44 -0400

    Create CalculatorCode.java

---
+++ CalculatorCode.java
@@ -0,0 +1,63 @@
+/* 
+    CalculatorCode.java   Author: Joshua Rabbani
+    
+    Source code for basic calculatory functions. 
+    Functions include: 
+      * Addition
+      * Subtraction
+      * Division
+      * Multiplication
+      * Square root (possible any root)
+      * Logarithms
+      * x to the power of y
+      * Squaring
+*/
+import java.util.*; 
+public class CalculatorCode {
+  
+  public static addNumbers(double a, double b) {
+    
+    Scanner scanDouble = new Scanner (System.in);
+    a = scanDouble.nextDouble(); 
+    b = scanDouble.nextDouble(); 
+    
+    return a+b; 
+
+  public static subtractNumbers(double a, double b) {
+    
+    Scanner scanDouble = new Scanner (System.in);
+    a = scanDouble.nextDouble(); 
+    b = scanDouble.nextDouble(); 
+   
+    return a-b; 
+  }
+    
+  public static multiplyNumbers(double a, double b) {
+   
+    Scanner scanDouble = new Scanner (System.in);
+    a = scanDouble.nextDouble(); 
+    b = scanDouble.nextDouble(); 
+   
+    return a*b; 
+  }
+    
+  public static divideNumbers(double a, double b) {
+  
+    Scanner scanDouble = new Scanner (System.in);
+    a = scanDouble.nextDouble(); 
+    b = scanDouble.nextDouble(); 
+  
+    return a/b; 
+  }
+    
+  public static xPowOfy(double a, double b) {
+  
+    Scanner scanDouble = new Scanner (System.in);
+    a = scanDouble.nextDouble(); 
+    b = scanDouble.nextDouble(); 
+   
+    return Math.pow(a, b); 
+  }
+    
+   }
+}
